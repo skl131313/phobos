a0d0952affaca7d9802db052685e16bffc3cc07d
sprinkle131313 <sprinkle131313@users.noreply.github.com>
2017-01-28 11:52:56 -0500

    Add export to std.traits.

--- std/traits.d
+++ std/traits.d
@@ -161,7 +161,7 @@
  *    (See accompanying file LICENSE_1_0.txt or copy at
  *          http://www.boost.org/LICENSE_1_0.txt)
  */
-module std.traits;
+export module std.traits;
 
 import std.typetuple; // TypeTuple
 
